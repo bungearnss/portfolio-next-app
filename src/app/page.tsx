2482844f826289c586a671e4ee881d80487c62b3
bungearntun <love2554f@hotmail.com>
2024-07-31 21:24:48 +0700

    updated : page with skill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import Header from "./Header";
 import Introduce from "./feature/introduce/Introduce";
 import About from "./feature/about/About";
+import Skill from "./feature/skill/Skill";
 
 export default function Home() {
   return (
@@ -12,8 +12,11 @@ export default function Home() {
       <div className="mt-10 w-full max-w-5xl items-center justify-start lg:flex">
         <Introduce />
       </div>
-      <div className="bg-custom-bg mt-10 w-full max-w-10xl items-center justify-start py-10 lg:flex">
+      <div className="bg-custom-bg max-w-10xl mt-10 w-full items-center justify-start py-10 lg:flex">
         <About />
+      </div>
+      <div className="max-w-10xl w-full items-center justify-start py-10 lg:flex">
+        <Skill />
       </div>
     </main>
   );
