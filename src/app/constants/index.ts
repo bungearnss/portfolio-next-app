53f94ea29daf67e36a82cd52e6489ee6f347f612
bungearntun <love2554f@hotmail.com>
2024-08-01 11:04:57 +0700

    updated : constants

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,9 +1,14 @@
 export const PROFILE = "/assets/images/profile.jpg";
 export const IN_ICON = "/assets/icons/linkedin.png";
+export const CALENDAR_ICON ="/assets/icons/calendar.png";
+
+/* Work Experience */
 export const GIT_ICON = "/assets/icons/git.png";
 export const ACN_LOGO = "/assets/images/accenture-logo.png";
 export const DELOITTE_LOGO = "/assets/images/deloitte-Logo.png";
 export const KBTG_LOGO = "/assets/images/kbtg-logo.png";
+
+/* Skill */
 export const SPRING_LOGO = "/assets/images/spring-logo.png";
 export const NODE_LOGO = "/assets/images/node-logo.png";
 export const NEXTJS_LOGO = "/assets/images/nextjs-logo.png";
@@ -12,5 +17,13 @@ export const FLUTTER_LOGO = "/assets/images/flutter-logo.png";
 export const GRPC_LOGO = "/assets/images/grpc-logo.png";
 export const POSTGRESQL_LOGO = "/assets/images/postgresql-logo.png";
 export const AZURE_LOGO = "/assets/images/azure-logo.png";
+
+/* Work */
+export const KF_SHABU = "/assets/images/experience/kungfu-shabu.png";
+export const ALOO = "/assets/images/experience/aloo.png";
+export const AMAZON_CLONE = "/assets/images/experience/amazon-clone.png";
+export const CLOVER_SUPPLY = "/assets/images/experience/cloversupply.png";
+export const ORG = "/assets/images/experience/organization.png";
+
 export const LINKEDIN_LINK = "https://www.linkedin.com/in/tunsuda-boonloed-735b1220a/";
 export const GIT_LINK = "https://github.com/bungearnss";
