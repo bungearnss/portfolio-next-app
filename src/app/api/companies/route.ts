fa2792a19cc5f71dc47d403d9199676b1063ac55
bungearntun <love2554f@hotmail.com>
2024-08-02 14:25:52 +0700

    added : api for fetch company data

---
+++ src/app/api/companies/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import clientPromise from '../../server/mongo/MongoClient';
+
+export async function GET() {
+  const dbName = process.env.MONGODB_NAME!;
+  const collectionName = process.env.MONGODB_COL_COMP!;
+  
+  try {
+    const client = await clientPromise;
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const data = await collection.find({}).toArray();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+  }
+}
