e6674212cb273f9136025adb154287fc5b362d37
bungearntun <love2554f@hotmail.com>
2024-08-02 07:52:48 +0700

    added: footer page

---
+++ src/app/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black px-24 py-14 text-white">
+      <div className="mx-auto flex max-w-5xl flex-col items-center justify-center">
+        <p className="mb-2 text-lg">Have a project ?</p>
+        <p className="my-4 text-5xl">Let's talk with me</p>
+        <p>Email: bungearntun@gmail.com</p>
+        <p>Phone: +66 64-836-1589</p>
+      </div>
+    </footer>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,49 @@ import About from "./feature/about/About";
 import Skill from "./feature/skill/Skill";
 import Experience from "./feature/experience/Experience";
 import Certificate from "./feature/certificate/Certificate";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start bg-white px-24 py-10">
-      <div className="z-10 w-full max-w-5xl">
-        <Header />
+    <div className="flex min-h-screen flex-col">
+      <main className="flex min-h-screen flex-col items-center justify-start bg-white px-24 pt-10">
+        <div className="z-10 w-full max-w-5xl sm:hidden">
+          <Header />
+        </div>
+        <div
+          id="introduce"
+          className="mt-10 w-full max-w-5xl items-center justify-start lg:flex"
+        >
+          <Introduce />
+        </div>
+        <div
+          id="about"
+          className="mt-10 w-screen items-center justify-start bg-custom-bg py-10 lg:flex"
+        >
+          <About />
+        </div>
+        <div
+          id="skill"
+          className="max-w-10xl w-full items-center justify-start py-10 lg:flex"
+        >
+          <Skill />
+        </div>
+        <div
+          id="experience"
+          className="max-w-10xl w-screen items-center justify-start bg-custom-bg py-10 lg:flex"
+        >
+          <Experience />
+        </div>
+        <div
+          id="certificate"
+          className="max-w-10xl w-screen items-center justify-start bg-custom-bg py-10 lg:flex"
+        >
+          <Certificate />
+        </div>
+      </main>
+      <div id="footer">
+        <Footer />
       </div>
-      <div className="mt-10 w-full max-w-5xl items-center justify-start lg:flex">
-        <Introduce />
-      </div>
-      <div className="bg-custom-bg max-w-10xl mt-10 w-full items-center justify-start py-10 lg:flex">
-        <About />
-      </div>
-      <div className="max-w-10xl w-full items-center justify-start py-10 lg:flex">
-        <Skill />
-      </div>
-      <div className="bg-custom-bg max-w-10xl w-full items-center justify-start py-10 lg:flex">
-        <Experience />
-      </div>
-      <div className="bg-custom-bg max-w-10xl w-full items-center justify-start py-10 lg:flex">
-        <Certificate />
-      </div>
-    </main>
+    </div>
   );
 }
