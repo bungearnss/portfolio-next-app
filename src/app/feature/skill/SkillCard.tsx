3ce52ada13be359911a18603357e40e801d9ca93
bungearntun <love2554f@hotmail.com>
2024-08-02 07:52:32 +0700

    added : tooltip skill card

--- src/app/feature/skill/SkillCard.tsx
+++ src/app/feature/skill/SkillCard.tsx
@@ -9,34 +9,53 @@ interface SkillCardProps {
 }
 
 export default function SkillCard({ icon, imgName, id }: SkillCardProps) {
-    const numericId = parseInt(id);
-    let borderClass = '';
-    let bgColorClass = '';
-  
-    if ([1, 2, 3, 4].includes(numericId)) {
-      bgColorClass = numericId % 2 === 0 ? 'bg-custom-bg' : 'bg-white';
-    } else {
-      bgColorClass = numericId % 2 === 0 ? 'bg-white' : 'bg-custom-bg';
-    }
-  
-    switch (true) {
-      case [5, 6, 7].includes(numericId):
-        borderClass = 'border border-t-0 border-l-0 border-r border-b-0 border-border-custom';
-        break;
-      case numericId === 1:
-        borderClass = 'border border-t-0 border-l-0 border-r-0 border-b border-border-custom';
-        break;
-      case numericId === 8:
-        borderClass = 'border border-t-0 border-l-0 border-r-0 border-b-0 border-border-custom';
-        break;
-      default:
-        borderClass = 'border border-t-0 border-l-0 border-r border-b border-border-custom';
-        break;
-    }
+  const numericId = parseInt(id);
+  let borderClass = "";
+  let bgColorClass = "";
+  let tooltipSty = "";
+
+  if ([1, 2, 3, 4].includes(numericId)) {
+    bgColorClass = numericId % 2 === 0 ? "bg-custom-bg" : "bg-white";
+    tooltipSty = "absolute bottom-full mb-2 rounded bg-gray-500 px-2 py-1 text-xs text-white";
+  } else {
+    bgColorClass = numericId % 2 === 0 ? "bg-white" : "bg-custom-bg";
+    tooltipSty = "absolute top-full mt-2 rounded bg-gray-500 px-2 py-1 text-xs text-white";
+  }
+
+  switch (true) {
+    case [5, 6, 7].includes(numericId):
+      borderClass =
+        "border border-t-0 border-l-0 border-r border-b-0 border-border-custom";
+      break;
+    case numericId === 1:
+      borderClass =
+        "border border-t-0 border-l-0 border-r-0 border-b border-border-custom";
+      break;
+    case numericId === 8:
+      borderClass =
+        "border border-t-0 border-l-0 border-r-0 border-b-0 border-border-custom";
+      break;
+    default:
+      borderClass =
+        "border border-t-0 border-l-0 border-r border-b border-border-custom";
+      break;
+  }
+
+  const [tooltip, setTooltip] = useState("");
+
+  const showTooltip = (tooltipText: string) => {
+    setTooltip(tooltipText);
+  };
+
+  const hideTooltip = () => {
+    setTooltip("");
+  };
 
   return (
     <div
       className={`${bgColorClass} flex transform items-center justify-center p-4 py-6 ${borderClass}`}
+      onMouseEnter={() => showTooltip(imgName)}
+      onMouseLeave={hideTooltip}
     >
       <div className="flex items-center justify-center">
         <Image
@@ -45,6 +64,11 @@ export default function SkillCard({ icon, imgName, id }: SkillCardProps) {
           width={70}
           height={70}
         />
+         {tooltip === imgName && (
+              <div className={tooltipSty}>
+                {imgName}
+              </div>
+            )}
       </div>
     </div>
   );
