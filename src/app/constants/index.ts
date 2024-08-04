b630d37f1dc7170078de89d40045332e54ad104a
bungearntun <love2554f@hotmail.com>
2024-08-04 13:42:06 +0700

    updated : integrate new component

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -3,6 +3,8 @@ export const IN_ICON = "/assets/icons/linkedin.png";
 export const CALENDAR_ICON ="/assets/icons/calendar.png";
 export const CLOSE_ICON = "/assets/icons/close.png";
 export const MENU_ICON = "/assets/icons/menu.png";
+export const ARROW_UP = "/assets/icons/arrow-up.png";
+export const ARROW_DOWN= "/assets/icons/down-arrow.png";
 
 /* Work Experience */
 export const GIT_ICON = "/assets/icons/git.png";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Header from "./Header";
 import Introduce from "./feature/introduce/Introduce";
 import About from "./feature/about/About";
@@ -5,14 +6,21 @@ import Skill from "./feature/skill/Skill";
 import Experience from "./feature/experience/Experience";
 import Certificate from "./feature/certificate/Certificate";
 import Footer from "./Footer";
+import FloatingActionButton from "./components/FloatingActionButton";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex flex-col items-center justify-start bg-white px-4 pt-6 md:px-24 md:pt-10 min-h-screen">
         <div className="z-10 w-full max-w-4xl md:max-w-5xl">
           <Header />
         </div>
+
         <div
           id="introduce"
           className="mt-16 md:mt-10 w-full max-w-5xl items-center justify-start lg:flex"
@@ -47,6 +55,7 @@ export default function Home() {
       <div id="footer">
         <Footer />
       </div>
+      <FloatingActionButton />
     </div>
   );
 }
